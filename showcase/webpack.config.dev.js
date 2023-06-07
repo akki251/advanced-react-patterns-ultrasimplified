e5da961590faab3c980de180abefa65b0f7670d4
akshansh <akshansh773@gmail.com>
2023-06-07 23:20:25 +0530

    updated webpack config

--- showcase/webpack.config.dev.js
+++ showcase/webpack.config.dev.js
@@ -1,17 +1,20 @@
-const merge = require("webpack-merge");
-const baseConfig = require("./webpack.config.base");
-
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.config.base');
+const crypto = require('crypto');
+const crypto_orig_createHash = crypto.createHash;
+crypto.createHash = (algorithm) =>
+  crypto_orig_createHash(algorithm == 'md4' ? 'sha256' : algorithm);
 module.exports = merge(baseConfig, {
-  mode: "development",
+  mode: 'development',
   devServer: {
     port: 4646,
     open: true,
     overlay: {
       warnings: true,
-      errors: true
+      errors: true,
     },
     historyApiFallback: true,
-    hot: true
+    hot: true,
   },
-  devtool: "source-map"
+  devtool: 'source-map',
 });
